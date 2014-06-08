e4bb27cf612eda5bac0febfeda8c1f38be7107f2
pabloale <pabloale_1302@hotmail.com>
2014-06-08 01:19:28 -0300

    Tp3 ej4 : utils validaciones + links

--- TP3/ejercicio4/client.c
+++ TP3/ejercicio4/client.c
@@ -1,10 +1,22 @@
+/*#####################################
+#Trabajo Práctico Nº3
+#Ejercicio Número 4
+#Arias, Rodrigo DNI: 34.712.865
+#Culen, Fernando DNI: 35.229.859
+#García Alves, Pablo DNI: 34.394.775
+#Juffar, Sebastian DNI: 34.497.148
+#Nogueiras, Jorge DNI: 34.670.613
+#PRIMERA ENTREGA
+#####################################*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
 #include <netdb.h> 
 #define BUFFERSIZE 256
 void error(const char *msg)
--- TP3/ejercicio4/server.c
+++ TP3/ejercicio4/server.c
@@ -1,12 +1,24 @@
+/*#####################################
+#Trabajo Práctico Nº3
+#Ejercicio Número 4
+#Arias, Rodrigo DNI: 34.712.865
+#Culen, Fernando DNI: 35.229.859
+#García Alves, Pablo DNI: 34.394.775
+#Juffar, Sebastian DNI: 34.497.148
+#Nogueiras, Jorge DNI: 34.670.613
+#PRIMERA ENTREGA
+#####################################*/
+
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
 #define BUFFERSIZE 1000
 void error(const char *msg)
 {
