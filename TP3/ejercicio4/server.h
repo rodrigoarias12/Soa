07d467fbbcd9cdf8711752dcc2beee6e598f7345
fncnet <fncnet@yahoo.com.ar>
2014-06-10 22:18:56 -0300

    Tp 3 Ej 4 Mas avanzado
    +Cuando matas el server se queda esperando un ACEPT mas
    +Cuando pedis el log no lo devuelve
    +Cuando se deslogean todos los usuarios no muestra el log completo
    +Cuando cierra un cliente abruptamente se desconecta el server
    +Modularizar
    +Generar librerias con las funciones reutilizables

---
+++ TP3/ejercicio4/server.h
@@ -0,0 +1,35 @@
+/*#####################################
+#Trabajo Práctico Nº3
+#Ejercicio Número 4
+#Arias, Rodrigo DNI: 34.712.865
+#Culen, Fernando DNI: 35.229.859
+#García Alves, Pablo DNI: 34.394.775
+#Juffar, Sebastian DNI: 34.497.148
+#Nogueiras, Jorge DNI: 34.670.613
+#PRIMERA ENTREGA
+#####################################*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <signal.h>
+#include <pthread.h>
+#include <time.h>
+
+#include "utils.validaciones.c"
+
+#define BUFFERSIZE 1000
+
+void help();
+void imprimirError(int codigo, const char *msg);
+void armarEsqueletoDemonio();
+void *manejadorCliente(void *argumentos);
+void terminarServer(int signal);
